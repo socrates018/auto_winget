eb5b07b733a65644734e95c302916396bc5e16e4
socrates018 <socratessuciu8@gmail.com>
2025-05-20 14:01:21 +0300

    add batch version

---
+++ winget.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Elevate to admin if not already
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /b
+)
+
+:: Run winget upgrade silently, accept all agreements, no interaction
+winget upgrade --all --accept-package-agreements --accept-source-agreements --silent --include-unknown --force --disable-interactivity
+exit /b
